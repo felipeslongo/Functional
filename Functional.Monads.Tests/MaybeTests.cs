9eb8f48f3c904b9e397d1c2e1593d733248180bb
Felipe de Souza Longo <felipeslongo@gmail.com>
2018-08-02 19:29:26 -0300

    Created Maybe monad
    Created Maybe monad

---
+++ Functional.Monads.Tests/MaybeTests.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Felipeslongo.Functional.Monads.Tests
+{
+    class MaybeTests
+    {
+        [Fact]
+        public void Bind_ExceptionToAnotherException()
+        {
+            var maybe = new Maybe<Exception>(new Exception())
+                .Bind<ArgumentException>(e => new ArgumentException("", e));
+
+            Assert.IsType<ArgumentException>(maybe.Value);
+        }
+
+        [Fact]
+        public void Bind_NoneToOtherType_NullAndSafeNavigation()
+        {
+            var maybe = Maybe<Exception>.None()
+                .Bind<ArgumentException>(e => new ArgumentException("", e));
+
+            Assert.Null(maybe.Value);
+        }
+
+        [Fact]
+        public void Constructor_HasValue_IsNotNull()
+        {
+            var maybe = new Maybe<Exception>(new Exception());
+
+            Assert.True(maybe.HasValue);
+            Assert.False(maybe.IsNull);
+        }
+
+
+        [Fact]
+        public void None_IsNull_DoesNotHasValue()
+        {
+            var maybe = Maybe<Exception>.None();
+
+            Assert.False (maybe.HasValue);
+            Assert.True(maybe.IsNull);
+        }
+
+        [Fact]
+        public void WhenNull_DoNullAction_DontDoValueAction()
+        {
+            var maybe = Maybe<Exception>.None();
+
+            var nullActionExecuted = false;
+            maybe.WhenNull(() => nullActionExecuted = true);
+            Assert.True(nullActionExecuted);
+            maybe.WhenHasValue(e => Assert.True(false));
+        }
+
+    }
+}
---
+++ Functional.Monads/Maybe.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Felipeslongo.Functional.Monads
+{
+    /// <summary>
+    /// Functional-first language F# typically doesn't allow null for its types. 
+    /// Instead, F# has a maybe implementation built into the language: it's called option type.
+    /// </summary>
+    /// <typeparam name="T">Wrapped type</typeparam>
+    /// <seealso cref="https://mikhail.io/2018/07/monads-explained-in-csharp-again/">Credits to Mikhail Shilkov,</seealso>
+    public class Maybe<T> where T : class
+    {
+        public T Value { get; private set; }
+        public bool HasValue => !IsNull;
+        public bool IsNull => Value == null;
+
+        public Maybe(T someValue)
+        {
+            this.Value = someValue ?? throw new ArgumentNullException(nameof(someValue));
+        }
+
+        private Maybe()
+        {
+        }
+
+        public Maybe<U> Bind<U>(Func<T, Maybe<U>> func) where U : class
+        {
+            return Value != null ? func(Value) : Maybe<U>.None();
+        }
+
+        public static Maybe<T> None() => new Maybe<T>();
+
+        public static implicit operator Maybe<T>(T arg) => new Maybe<T>(arg);
+
+        public void WhenNull(Action action)
+        {
+            if(action == null)
+                throw new ArgumentNullException(nameof(action));
+
+            if (IsNull)
+                action();
+        }
+
+        public void WhenHasValue(Action<T> action)
+        {
+            if (action == null)
+                throw new ArgumentNullException(nameof(action));
+
+            if (HasValue)
+                action(Value);
+        }
+
+    }
+}
